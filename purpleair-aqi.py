2ca26221f0d3c781cd14c8dc9c7bb6b06f61b321
bennywij <benny.wijatno@gmail.com>
2023-01-21 12:43:27 -0800

    A version without requests module
    still could use a refactor

--- purpleair-aqi.py
+++ purpleair-aqi.py
@@ -6,20 +6,44 @@ Created on Mon Jun  6 10:32:36 2022
 @author: benny
 """
 
+PA_API_Key = "8AF289D3-3772-11EC-B42E-42010A800002"
+PA_URL = "https://api.purpleair.com/v1/sensors/128231"
 
-import requests
+try:
+    import requests
+    headers = {"X-API-Key": PA_API_Key}  
+    response = requests.get(PA_URL, headers=headers)   
+    pm25 = response.json()['sensor']['stats']['pm2.5']
+    try: 
+    	humidity = response.json()['sensor']['humidity']
+    except:
+    	humidity = 75
 
-PA_API_Key = "YOUR_PA_KEY_HERE"
-PA_URL = "https://api.purpleair.com/v1/sensors/NUMERIC_SENSOR_ID_HERE"
+except ImportError:
+    # without requests
+    import json
+    from urllib.request import Request, urlopen
+    req = Request(PA_URL)
+    req.add_header('X-API-Key', PA_API_Key)
+    content = urlopen(req).read()
+    encoding = urlopen(req).info().get_content_charset('utf-8')
+    response = json.loads(content.decode(encoding))
 
-headers = {"X-API-Key": PA_API_Key}  
-response = requests.get(PA_URL, headers=headers)
- 
-pm25 = response.json()['sensor']['stats']['pm2.5']
-humidity = response.json()['sensor']['humidity']
+    pm25 = response['sensor']['stats']['pm2.5']
+
+    try: 
+    	humidity = response['sensor']['humidity']
+    except:
+    	humidity = 75
+
+
+
+
+
 
 # formula from https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwi72OSY0Jn4AhUim44IHWBHBxQQFnoECAQQAQ&url=https%3A%2F%2Fcfpub.epa.gov%2Fsi%2Fsi_public_file_download.cfm%3Fp_download_id%3D540979%26Lab%3DCEMM&usg=AOvVaw1VrPwqsac3Vh-XXGPGbkfG
 epa_adjusted_pm25 = pm25*0.52 + (humidity*-0.085) + 5.71
+
 
 def calculate_AQI (pm) :
     

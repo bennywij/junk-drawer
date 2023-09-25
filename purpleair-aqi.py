90f907dba69d254a4baca6c39984639b76627e39
bennywij <benny.wijatno@gmail.com>
2023-09-25 11:41:35 -0700

    Update purpleair-aqi.py with new EPA humidity adjustment calculation

--- purpleair-aqi.py
+++ purpleair-aqi.py
@@ -39,7 +39,24 @@ except:
 
 
 # formula from https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwi72OSY0Jn4AhUim44IHWBHBxQQFnoECAQQAQ&url=https%3A%2F%2Fcfpub.epa.gov%2Fsi%2Fsi_public_file_download.cfm%3Fp_download_id%3D540979%26Lab%3DCEMM&usg=AOvVaw1VrPwqsac3Vh-XXGPGbkfG
-epa_adjusted_pm25 = pm25*0.52 + (humidity*-0.085) + 5.71
+# epa_adjusted_pm25 = pm25*0.52 + (humidity*-0.085) + 5.71
+
+#revised formula from https://cfpub.epa.gov/si/si_public_file_download.cfm?p_download_id=544231&Lab=CEMM page 26
+
+def adjust_pm_25 (pm : float, humidity : float) :
+    
+    if pm < 30:
+        adj_pm = 0.524*pm - 0.0862*humidity + 5.75
+    elif 30 <= pm < 50:
+        adj_pm = (0.786 * (pm/20 - 3/2) + 0.524*(1 - (pm/20 - 3/2)))*pm - 0.0862*humidity + 5.75
+    elif 50 <= pm < 210:
+        adj_pm = 0.786*pm - 0.0862*humidity + 5.75
+    elif 210 <= pm < 260:
+        adj_pm = (0.69*(pm/50 - 21/5) + 0.786*(1 - (pm/50 - 21/5)))*pm - 0.0862*humidity*(1 - (pm/50 - 21/5)) + 2.966*(pm/50 - 21/5) + 5.75*(1 - (pm/50 - 21/5)) + 8.84*(10**-4)*pm**2*(pm/50 - 21/5)
+    elif 260 <= pm:
+        adj_pm = 2.966 + 0.69*pm + 8.84*10**-4*pm**2 
+        
+    return adj_pm
 
 
 def calculate_AQI (pm) :
@@ -61,5 +78,6 @@ def calculate_AQI (pm) :
 
     return round(aqi)
 
+epa_adjusted_pm25 = adjust_pm_25(pm25, humidity)
 
 print("☁︎ "+str(calculate_AQI(epa_adjusted_pm25)))
